6cbcd514b5f5d8251513375f401367dfb4482539
MD Iubullah Biplobe <biplobe6@gmail.com>
2018-10-07 06:16:11 +0600

    Added: Income, IncomeType in models

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class IncomeType(models.Model):
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.type)
+
+
+class Income(models.Model):
+    type = models.ForeignKey(IncomeType)
+    date = models.DateField()
+    amount = models.FloatField()
+
+    def __str__(self):
+        return str("type: {}, amount: {}, date: {}".format(self.type, self.amount, self.date))
