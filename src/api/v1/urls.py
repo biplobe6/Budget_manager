b31e8b0d161eaaeee509748354dcb8bb825bfa41
MD Iubullah Biplobe <biplobe6@gmail.com>
2018-10-08 14:15:08 +0600

    Modified: Income type url pattern

--- src/api/v1/urls.py
+++ src/api/v1/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url("^income$", views.income.get, name="list_income"),
     url("^income/add$", views.income.Add.as_view(), name="add_income"),
     url("^income/(?P<id>\d+)$", views.income.Details.as_view(), name="edit_income"),
-    url("^income_type$", views.incometype.List.as_view(), name="list_income_type"),
-    url("^income_type/add$", views.incometype.Add.as_view(), name="add_income_type"),
-    url("^income_type/(?P<id>\d+)$", views.incometype.Details.as_view(), name="details_income_type")
+    url("^income/type$", views.incometype.List.as_view(), name="list_income_type"),
+    url("^income/type/add$", views.incometype.Add.as_view(), name="add_income_type"),
+    url("^income/type/(?P<id>\d+)$", views.incometype.Details.as_view(), name="details_income_type")
 ]
