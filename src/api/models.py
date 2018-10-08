1979f7890b2047e17f681db22e3f8600ea8f99d0
MD Iubullah Biplobe <biplobe6@gmail.com>
2018-10-08 14:19:25 +0600

    Added: expense model

--- src/api/models.py
+++ src/api/models.py
@@ -16,3 +16,19 @@ class Income(models.Model):
 
     def __str__(self):
         return str("type: {}, amount: {}, date: {}".format(self.type, self.amount, self.date))
+
+
+class ExpenseType(models.Model):
+    type = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return str(self.type)
+
+
+class Expense(models.Model):
+    type = models.ForeignKey(ExpenseType, on_delete=models.CASCADE)
+    date = models.DateField(default=date.today)
+    amount = models.FloatField()
+
+    def __str__(self):
+        return str("type: {}, amount: {}, date: {}".format(self.type, self.amount, self.date))
