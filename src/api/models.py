945c8bd5bf55744b9776c36d0e2af7263748b297
MD Iubullah Biplobe <biplobe6@gmail.com>
2018-10-08 06:02:20 +0600

    Fixed: income type unique

--- src/api/models.py
+++ src/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class IncomeType(models.Model):
-    type = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return str(self.type)
