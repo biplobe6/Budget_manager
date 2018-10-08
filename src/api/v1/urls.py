89926be681739e54d56a1e787f154341342a017d
MD Iubullah Biplobe <biplobe6@gmail.com>
2018-10-08 06:04:31 +0600

    Removed: income functionality

--- src/api/v1/urls.py
+++ src/api/v1/urls.py
@@ -4,7 +4,5 @@ from . import views
 app_name = "v1"
 
 urlpatterns = [
-    url(r'^income/get', views.income.get, name="get_income"),
-    url(r'^income/add', views.income.add, name="add_income"),
     url(r'^hello', views.hello.index, name="hello")
 ]
